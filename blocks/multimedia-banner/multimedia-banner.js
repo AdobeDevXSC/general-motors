fd6bcd4f13632f0677886dc2f8f1df3ddaddeda0
timwlee <timothyl@adobe.com>
2025-05-29 16:12:16 -0700

    Update multimedia-banner

--- blocks/multimedia-banner/multimedia-banner.js
+++ blocks/multimedia-banner/multimedia-banner.js
@@ -1,14 +1,20 @@
 import { makeVideo } from '../../scripts/scripts.js';
 
-export default async function decorate(block) {  
-  const videos = block.querySelectorAll('div a');
+export default async function decorate(block) {
+  // Structure the block
+  const [mediaContainer, infoContainer] = block.children;
+  mediaContainer?.classList.add('media-container');
+  infoContainer?.classList.add('information');
 
-  videos.forEach((video) => {
-    video.href = video.href;
-    makeVideo(video.parentElement.parentElement, video.href);
-    video.remove();
-  });
+      infoContainer?.classList.add('align-top-left');
 
-  block.querySelector(':scope > div:nth-of-type(1)').classList.add('video-container');
-  block.querySelector(':scope > div:nth-of-type(2)').classList.add('information');
+
+  // Check for media type
+  const videoLink = mediaContainer.querySelector('a');
+
+  if (videoLink?.href) {
+    // Handle video
+    makeVideo(mediaContainer, videoLink.href);
+    videoLink.remove();
+  } 
 }
